f1f9e9bd780383fdd4c02d0adb1156c68cee2f3e
Linguistics Student's GitHub <student.linguistics@gmail.com>
2017-03-21 23:15:03 -0400

    Update chapter_1_twitter.py

--- chapter_1_twitter.py
+++ chapter_1_twitter.py
@@ -1,5 +1,8 @@
 # Chapter 1 Mining the Social Web 2nd Ed
-
+#############################################################
+# 1.3.3 Exploring Trending Topics
+#############################################################
+ 
 import twitter
 from credentials import *
 
@@ -36,3 +39,29 @@ common_trends = world_trends_set.intersection(us_trends_set)
 print common_trends
 print world_trends_set
 print us_trends_set
+
+#############################################################
+# 1.3.4 Searching for Tweets
+#############################################################
+
+q = '#ChicagoFire'
+count = 100
+search_results = twitter_api.search.tweets(q=q, count=count)
+statuses = search_results['statuses']
+
+
+# Iterate through 5 more batches of results by following the cursor
+for _ in range(5):
+    print "Length of statuses", len(statuses)
+    try:
+        next_results = search_results['search_metadata']['next_results']
+        # Create a dictionary from next_results, which has the following form:
+        # ?max_id=844373502757158913&q=%2523ChicagoFire&count=100&include_entities=1
+        kwargs = dict([ kv.split('=') for kv in next_results[1:].split("&") ])
+        search_results = twitter_api.search.tweets(**kwargs)
+        statuses += search_results['statuses']
+    except KeyError, e: # No more results when next_results doesn't exist
+        break
+
+# Show one sample search result by slicing the list...
+print json.dumps(statuses[0], indent=1)
