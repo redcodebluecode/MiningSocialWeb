64eef76abd90ad1d8a63d8141df728fb28d7f249
Linguistics Student's GitHub <student.linguistics@gmail.com>
2017-03-27 13:00:24 -0400

    Update chapter_1_twitter.py

--- chapter_1_twitter.py
+++ chapter_1_twitter.py
@@ -85,6 +85,10 @@ print json.dumps(screen_names[0:5], indent=1)
 print json.dumps(hashtags[0:5], indent=1)
 print json.dumps(words[0:5], indent=1)
 
+#############################################################
+# 1.4.2 Tweets Entities with Frequency Analysis
+#############################################################
+
 from collections import Counter
 for item in [words, screen_names, hashtags]:
     c = Counter(item)
@@ -99,6 +103,10 @@ for label, data in (('Word', words), ('Screen Name', screen_names), ('Hashtag', 
     pt.align[label], pt.align['Count'] = 'l', 'r' # Set column alignment
     print pt
 
+#############################################################
+# 1.4.3 Computing the Lexical Diversity of Tweets
+#############################################################    
+    
 # A function for computing lexical diversity
 def lexical_diversity(tokens):
     return 1.0*len(set(tokens))/len(tokens)
@@ -111,3 +119,72 @@ print lexical_diversity(words)
 print lexical_diversity(screen_names)
 print lexical_diversity(hashtags)
 print average_words(status_texts)
+
+#############################################################
+# 1.4.4 Finding the Most Popular Retweets
+############################################################# 
+
+retweets = [
+# Store out a tuple of these three values ...
+(status['retweet_count'],
+status['retweeted_status']['user']['screen_name'],
+status['text'])
+
+# ... for each status ...
+for status in statuses
+
+# ... so long as the status meets this condition.
+    if status.has_key('retweeted_status')
+]
+
+# Slice off the first 5 from the sorted results and display each item in the tuple
+
+pt = PrettyTable(field_names=['Count', 'Screen Name', 'Text'])
+[ pt.add_row(row) for row in sorted(retweets, reverse=True)[:5] ]
+pt.max_width['Text'] = 50
+pt.align= 'l'
+print pt
+
+#############################################################
+# 1.4.4 Looking Up Users Who Have Retweeted a Status
+############################################################# 
+
+# Get the original tweet id for a tweet from its retweeted_status node
+# and insert it here in place of the sample value that is provided
+# from the text of the book
+_retweets = twitter_api.statuses.retweets(id=317127304981667841)
+print [r['user']['screen_name'] for r in _retweets]
+
+#############################################################
+# 1.4.5 Visualizing Frequency Data with Histograms
+############################################################# 
+
+word_counts = sorted(Counter(words).values(), reverse=True)
+plt.loglog(word_counts)
+plt.ylabel("Freq")
+plt.xlabel("Word Rank")
+
+# Generating histograms of words, screen names, and hashtags
+for label, data in (('Words', words),
+('Screen Names', screen_names),
+('Hashtags', hashtags)):
+# Build a frequency map for each set of data
+# and plot the values
+    c = Counter(data)
+    plt.hist(c.values())
+# Add a title and y-label ...
+    plt.title(label)
+    plt.ylabel("Number of items in bin")
+    plt.xlabel("Bins (number of times an item appeared)")
+# ... and display as a new figure
+    plt.figure()
+
+# Generating a histogram of retweet counts
+# Using underscores while unpacking values in
+# a tuple is idiomatic for discarding them
+counts = [count for count, _, _ in retweets]
+plt.hist(counts)
+plt.title("Retweets")
+plt.xlabel('Bins (number of times retweeted)')
+plt.ylabel('Number of tweets in bin')
+print counts
